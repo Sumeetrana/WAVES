a0d7de1acb97b547a09f41a24b741de8f6ef5541
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-18 19:25:23 +0530

    Adding products route and model

---
+++ server/models/product.model.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 100000
+    },
+    price: {
+        type: Number,
+        required: true,
+        maxlength: 255
+    },
+    brand: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },
+    shipping: {
+        required: true,
+        type: Boolean
+    },
+    available: {
+        type: Boolean,
+        required: true
+    },
+    wood: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },
+    frets: {
+        required: true,
+        type: Number
+    },
+    sold: {
+        type: Number,
+        maxlength: 100,
+        default: 0
+    },
+    publish: {
+        required: true,
+        type: Boolean
+    },
+    images: {
+        type: Array,
+        default: []
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Product', productSchema)
--- server/routes/product.js
+++ server/routes/product.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth')
 const admin = require('../middleware/admin')
 const Brand = require('../models/brand.model')
 const Wood = require('../models/wood.model')
+const Product = require('../models/product.model')
 
 router.post('/brand', auth, admin, (req,res) => {
     const brand = new Brand(req.body);
@@ -52,6 +53,20 @@ router.get('/allwoods', (req, res) => {
             return res.status(400).send(err)
         }
         res.status(200).send(woods)
+    })
+})
+
+router.post('/article', (req, res) => {
+    const product = new Product(req.body);
+
+    product.save((err, doc) => {
+        if (err) {
+            return res.json({ success: false, err })
+        }
+        res.status(200).json({
+            success: true,
+            article: doc
+        })
     })
 })
 
