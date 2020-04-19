f09a75f27b9a23c47b3e8700b1bb5e5dd024e36e
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-19 11:15:57 +0530

    Header and Footer added

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Resources/css/styles.css'
 
-import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './routes';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
