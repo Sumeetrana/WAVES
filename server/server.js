b655c0d1e4a5793b7f3675adf3a5c3ad81def1eb
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-19 10:13:35 +0530

    Client side setup and created a procy server

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 app.use('/api/user', require('./routes/user'))
 app.use('/api/product', require('./routes/product'))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
