a3c674d48f076d041c743b69f5322cfecb866bba
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-19 12:10:02 +0530

    REDUX setup

---
+++ client/src/components/utils/misc.js
@@ -0,0 +1,2 @@
+export default USER_SERVER = '/api/user'
+export default PRODUCT_SERVER = '/api/product'
--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import './Resources/css/styles.css'
 
 import { BrowserRouter } from 'react-router-dom'
-
 import Routes from './routes';
 
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import promiseMiddleware from 'redux-promise'
+import thunk from 'redux-thunk'
+
+import Reducer from './Reducers'
+
+const store = createStore(Reducer, applyMiddleware(promiseMiddleware, thunk))
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
