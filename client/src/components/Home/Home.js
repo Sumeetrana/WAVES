05b283a931584502f3c5b89d6ea1571a730f67aa
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-21 10:21:43 +0530

    Home Cards-getproductsbysell and getproductsbyarrival functionality added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react'
 import HomeSlider from './home_slider'
 import HomePromotion from './home_promotion'
 
+import { connect } from 'react-redux'
+import { getProductsByArrival, getProductsBySell } from '../../actions/product_actions'
 class Home extends Component {
+
+    componentDidMount() {
+        this.props.dispatch(getProductsBySell()).then(response => console.log(this.props.products))
+        this.props.dispatch(getProductsByArrival()).then(response => console.log(this.props.products))
+    }
+
     render() {
         return (
             <div>
@@ -13,4 +21,10 @@ class Home extends Component {
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps)(Home)
